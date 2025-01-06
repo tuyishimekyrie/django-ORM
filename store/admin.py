6a88fe99370968798e56d9805876cb3f347dc389
tuyishimekyrie <tuyishimehope01@gmail.com>
2025-01-06 23:23:21 +0200

    add collection model to the admin site

--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
 from . import models
-
+from django.db.models.aggregates import Count
 # Register your models here.
+
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['title','products_count']
+    
+    @admin.display(ordering='products_count')
+    def products_count(self,collection):
+        return collection.products_count
+    
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(products_count =Count('product'))
+
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ["title","unit_price","inventory_status","collection_title"]
@@ -24,7 +36,7 @@ class CustomerAdmin(admin.ModelAdmin):
     ordering = ['first_name','last_name']
     list_per_page = 10
     
-admin.site.register(models.Collection)
+# admin.site.register(models.Collection)
 
 # admin.site.register(models.Product,ProductAdmin)
 

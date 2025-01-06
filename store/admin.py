3d176f90cf682352285f438039784144a05baf4e
tuyishimekyrie <tuyishimehope01@gmail.com>
2025-01-06 23:13:30 +0200

    Add order and customer models to admin site

--- store/admin.py
+++ store/admin.py
@@ -2,7 +2,35 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
-
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["title","unit_price","inventory_status","collection_title"]
+    list_editable = ['unit_price']
+    list_per_page = 10
+    list_select_related = ['collection']
+    
+    def collection_title(self,product):
+        return product.collection.title
+    
+    @admin.display(ordering='inventory')
+    def inventory_status(self,product):
+        if product.inventory < 10:
+            return 'Low'
+        return 'Ok'
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name','last_name','membership']
+    list_editable = ['membership']
+    ordering = ['first_name','last_name']
+    list_per_page = 10
+    
 admin.site.register(models.Collection)
 
-admin.site.register(models.Product)
+# admin.site.register(models.Product,ProductAdmin)
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id','placed_at','customer']
+    list_per_page = 10
+    
+   
--- store/models.py
+++ store/models.py
@@ -50,6 +50,12 @@ class Customer(models.Model):
     birth_date = models.DateField(null=True)
     membership = models.CharField(
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+    
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+    
+    class Meta:
+        ordering: ['first_name','last_name']
 
 
 class Order(models.Model):
@@ -66,6 +72,8 @@ class Order(models.Model):
     payment_status = models.CharField(
         max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+    
+    
 
 
 class OrderItem(models.Model):

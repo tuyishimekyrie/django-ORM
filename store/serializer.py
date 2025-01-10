3a99ef1c41f35c87b141d04263eb50bedefaf658
tuyishimekyrie <tuyishimehope01@gmail.com>
2025-01-10 11:47:15 +0200

    add model serializer (DRY)

--- store/serializer.py
+++ store/serializer.py
@@ -1,25 +1,30 @@
 from rest_framework import serializers
 from decimal import Decimal
-from .models import Product
+from .models import Product,Collection
 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length = 255)
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id','title']
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length = 255)
-    description = serializers.CharField(max_length = 255)
-    price = serializers.DecimalField(max_digits=9,decimal_places=2,source="unit_price")
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id','title','description','unit_price','price_with_tax','collection']
+
+    # id = serializers.IntegerField()
+    # title = serializers.CharField(max_length = 255)
+    # description = serializers.CharField(max_length = 255)
+    # price = serializers.DecimalField(max_digits=9,decimal_places=2,source="unit_price")
     price_with_tax = serializers.SerializerMethodField(method_name="calculate_tax")
-    # collection = serializers.PrimaryKeyRelatedField(
-    #     queryset = Product.objects.all()
-    # )
-    # collection = serializers.StringRelatedField()
-    # collection = CollectionSerializer()
-    collection = serializers.HyperlinkedRelatedField(
-        queryset = Product.objects.all(),
-        view_name = 'collection_detail'
-    )    
+    # # collection = serializers.PrimaryKeyRelatedField(
+    # #     queryset = Product.objects.all()
+    # # )
+    # # collection = serializers.StringRelatedField()
+    # # collection = CollectionSerializer()
+    # collection = serializers.HyperlinkedRelatedField(
+    #     queryset = Product.objects.all(),
+    #     view_name = 'collection_detail'
+    # )    
     def calculate_tax(self,product:Product):
         return product.unit_price * Decimal(1.1)

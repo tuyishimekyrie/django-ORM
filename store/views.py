549468c97739f5c4686c4b980fe00eb15b7c127d
tuyishimekyrie <tuyishimehope01@gmail.com>
2025-01-13 21:45:42 +0200

    add functionality to add item to cart

--- store/views.py
+++ store/views.py
@@ -12,7 +12,7 @@ from rest_framework.views  import APIView
 from rest_framework.viewsets import ModelViewSet,GenericViewSet
 from rest_framework.pagination import PageNumberPagination
 from .models import Collection, Product,OrderItem,Review,Cart,CartItem
-from .serializer import CollectionSerializer, ProductSerializer,ReviewSerializer,CartSerializer,CartItemSerializer
+from .serializer import CollectionSerializer, ProductSerializer,ReviewSerializer,CartSerializer,CartItemSerializer,AddCartItemSerializer
 from .filters import ProductFilter
 from .pagination import DefaultPagination
 
@@ -175,7 +175,15 @@ class CartViewSet(CreateModelMixin,RetrieveModelMixin,GenericViewSet,DestroyMode
     serializer_class = CartSerializer
 
 class CartItemViewSet(ModelViewSet):
-    serializer_class = CartItemSerializer
+    # serializer_class = CartItemSerializer
+    
+    def get_serializer_class(self):
+        if self.request.method == "POST":
+            return AddCartItemSerializer
+        return CartItemSerializer
+    
+    def get_serializer_context(self):
+        return {'cart_id':self.kwargs['cart_pk']}
     
     def get_queryset(self):
         return CartItem.objects.filter(cart_id = self.kwargs['cart_pk']).select_related("product")

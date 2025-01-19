8d48b111037e18a44ee35e6dfd367d49d06547c0
tuyishimekyrie <tuyishimehope01@gmail.com>
2025-01-19 21:46:17 +0200

    improve code quality

--- store/serializer.py
+++ store/serializer.py
@@ -6,7 +6,6 @@ class CollectionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Collection
         fields = ['id','title','products_count']
-        
     products_count = serializers.IntegerField(read_only=True)
 
 class ProductSerializer(serializers.ModelSerializer):
